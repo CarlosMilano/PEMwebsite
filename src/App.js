b86887d0d151e95a09c7fc90c78dae9efeeab3ee
CarlosMilano <a01383102@itesm.mx>
2023-06-27 21:04:04 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -69,42 +69,61 @@ function App() {
       <main>
         <section id="nosotros" >
           <h2 className='title'>Nosotros</h2>
-          <p>
+          <p className='nosotros'>
             Somos una empresa de Autotransporte que inicia sus operaciones en el año 2018, con el firme propósito de lograr la completa satisfacción de sus clientes, ofreciendo un servicio eficiente que solucione las necesidades de nuestros clientes, con el compromiso de siempre contar con la infraestructura necesaria para cumplir con los requerimientos de puntualidad y seguridad de sus cargas.
           </p>
         </section>
         <section id="info">
-        <div className="row-container">
+          <div className='contenedor-circulos'>
+          <div className="row-ventaja">
         <div className='circulo'>
-          <div className="subtitulo">
-              <h3>Misión</h3>
-              <p>
+          <div>
+              <h3 className="subtitulo">Misión</h3>
+              <p style={{textAlign: 'center'}}>
                 Ofrecer un servicio de autotransporte puntual, seguro y confiable. Mejorando continuamente mediante la aplicación de mejores prácticas y tecnologías, contribuyendo de esta manera en el éxito de nuestra empresa, de nuestros clientes y de nuestros colaboradores.
               </p>
             </div>
           </div>
             <div className='circulo'>
-            <div className="subtitulo">
-              <h3>Visión</h3>
-              <p>
+            <div>
+            <h3 className="subtitulo">Visión</h3>
+              <p style={{textAlign: 'center'}}>
                 Ser la empresa líder de la región en el sector de autotransporte, que se distinga por su atención al cliente y por su constante esfuerzo para superar y cumplir con todos sus requerimientos y expectativas.
               </p>
             </div>
             </div>
             </div>
+            <div className='row-ventaja'>
             <div className='circulo'>
-            <div className="subtitulo">
-              <h3>Valores</h3>
+              <div>
+              <h3 className="subtitulo">Valores</h3>
               <p>
-                Honestidad<br />
-                Lealtad<br />
-                Actitud de servicio<br />
-                Compromiso<br />
-                Profesionalismo<br />
-                Trabajo en equipo
+                - Honestidad<br />
+                - Lealtad<br />
+                - Actitud de servicio<br />
+                - Compromiso<br />
+                - Profesionalismo<br />
+                - Trabajo en equipo
               </p>
+              </div>
             </div>
             </div>
+          </div>
+        </section>
+        <section id="servicio">
+          <h2 className='title'>Servicio</h2>
+          <div className='row'>
+          <p className="left">
+              Automotriz
+          </p>
+          <div className='row-rectangle-left'></div>
+          </div>
+          <div className='row-reverse'>
+          <p className="right">
+              Manufacturera
+          </p>
+          <div className='row-rectangle'></div>
+          </div>
         </section>
         <section id="cobertura">
           <h2 className='title blue'>Cobertura</h2>
@@ -112,15 +131,8 @@ function App() {
           Se cuenta con presencia de equipo terrestre para movimientos nacionales, intra US, importación y exportación.
           </p>
         </section>
-        <section id="servicio">
-          <h2 className='title'>Servicio</h2>
-          <p>
-              Automotriz<br />
-              <br />Manufacturera<br />
-          </p>
-        </section>
         <section id="personal">
-          <h2 className='title blue'>Personal</h2>
+          <h2 className='title'>Personal</h2>
           <p>
           Contamos con conductores calificados, con amplia experiencia y capacidad para transportar puntualmente cargas en rutas nacionales e internacionales.<br />
           <br />El personal operativo está capacitado en sistemas de localización y sistemas para el control administrativo.<br />
@@ -128,21 +140,33 @@ function App() {
           </p>
         </section>
         <section id="ventajas">
-          <h2 className='title'>Ventajas</h2>
-          <p>
-          Unidades con GPS con Sistema de Bloqueo con monitoreo las 24 horas del dia.<br />
-          <br />Unidades de reciente modelo (2017 o superior) que cuentan con mantenimiento   
-          continuo y preventivo.<br />
-          <br />Comunicación constante con el cliente sobre avances del servicio.<br />
-          <br />El mejor trato de servicio al cliente<br />
-          <br />Alta tasa de efectividad en tiempos de servicio.
+          <h2 className='title blue'>Ventajas</h2>
+          <div className='row-ventaja'>
+          <p className='rectangle-2'>
+          Comunicación constante con el cliente sobre avances del servicio.
           </p>
+          <p className='rectangle-2'>
+          El mejor trato de servicio al cliente.
+          </p>
+          <p className='rectangle-2'>
+          Alta tasa de efectividad en tiempos de servicio.
+          </p>
+          </div>
+          <div className='row-ventaja'>
+          <p className='rectangle-2'>
+          Unidades con GPS con Sistema de Bloqueo con monitoreo las 24 horas del dia.
+          </p>
+          <p className='rectangle-2'>
+          Unidades de reciente modelo (2017 o superior) que cuentan con mantenimiento   
+          continuo y preventivo.
+          </p>
+          </div>
         </section>
         <section id="contacto">
-          <h2 className='title blue'>Contacto</h2>
-          <div className="row-container" style={{ alignItems: 'center' }}>
-            <img src={icon} alt='icon' width={50} height={50} />
-          <p style={{ marginLeft: '20px' }}>
+          <h2 className='title'>Contacto</h2>
+          <div className="row-contacto" >
+            <img src={icon} alt='icon' width={40} height={40} />
+          <p style={{ marginLeft: '15px' }}>
           ventas@pem-logistics.com
           </p>
           </div>
